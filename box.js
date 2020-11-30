076f12eb520aa6f2408200801a58f6e15349d91a
faheemanasrin <69379162+faheemanasrin@users.noreply.github.com>
2020-11-30 20:33:26 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+class Box {
+    constructor(x, y, width, height) {
+      
+   
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      
+  
+      rectMode(CENTER);
+      fill(255);
+      
+      rect(x, y, width, height);
+      
+    }
+  };
